e9d7bed32e621946b96d9e312c07106da62769a1
Steve Ariss <steve@pikari.io>
2026-04-14 11:34:45 -0400

    Add preview build commands and mode-specific output dir

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     proxy: {
@@ -10,6 +10,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: mode === 'preview' ? 'dist-preview' : 'dist',
   },
-});
+}));
